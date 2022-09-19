bb4d17dc0bf30548dcda937be4c9e5a764765270
Francisco Pacheco <francisco.pacheco@autoliv.com>
2022-09-18 19:12:55 -0500

    change the oreder of moveing in cmp

--- lua/plugins/config/cmp.lua
+++ lua/plugins/config/cmp.lua
@@ -14,8 +14,8 @@ cmp.setup({
     documentation = cmp.config.window.bordered()
   },
 	mapping = {
-		["<C-j>"] = cmp.mapping.select_prev_item(),
-		["<C-k>"] = cmp.mapping.select_next_item(),
+		["<C-k>"] = cmp.mapping.select_prev_item(),
+		["<C-j>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
 		["<C-Space>"] = cmp.mapping.complete(),
