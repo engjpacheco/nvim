a5bee3337c5e8d98c6f5342fc84643c99f8224e6
Francisco Pacheco <francisco.pacheco@autoliv.com>
2022-10-10 22:53:36 -0500

    adding pandoc template

--- lua/plugins/config/luasnip.lua
+++ lua/plugins/config/luasnip.lua
@@ -26,14 +26,12 @@ ls.add_snippets(nil, {
         },
         {
             text({"---",
-            "title: "}), insert(1, "note_title"), text({"", 
+            "title: "}), insert(1, "document name"), text({"", 
             "author: "}), insert(2, "author"), text({"", 
             "date: "}), func(date, {}), text({"",
-            "categories: ["}), insert(3, ""), text({"]",
-            "lastmod: "}), func(date, {}), text({"",
-            "tags: ["}), insert(4), text({"]",
-            "comments: true",
-            "---", ""}),
+            "tags: ["}), insert(3), text({"]",
+            "---", ""}), text({"",
+            "\\tableofcontents"}),
             insert(0)
           }),
     },
